c14e724c167b2be40a4da5f482066da2dba1c52a
Silje Raddum <kjempelodott@gmail.com>
2019-12-08 21:48:30 +0100

    organize

---
+++ aoc2019/src/goddamncomputer.rs
@@ -0,0 +1,86 @@
+use std::collections::VecDeque;
+
+pub struct Program(Vec<isize>);
+
+impl Program {
+    pub fn new(bytecode: Vec<isize>) -> Self {
+        Program(bytecode)
+    }
+
+    pub fn run(&self, stdin: Vec<isize>) -> isize {
+        let mut heap = self.0.clone();
+        let mut i = 0;
+        let mut stdin = VecDeque::from(stdin);
+        let mut stdout = VecDeque::new();
+
+        loop {
+            let mut z = heap[i];
+            let op = z % 100;
+            let im2 = (z as f64).log10() >= 4.;
+            if im2 { z -= 10000; }
+            let im1 = (z as f64).log10() >= 3.;
+            if im1 { z -= 1000; }
+            let im0 = (z as f64).log10() >= 2.;
+
+            match op {
+                1 => {
+                    let v1 = if im0 { heap[i+1] } else { heap[heap[i+1] as usize] };
+                    let v2 = if im1 { heap[i+2] } else { heap[heap[i+2] as usize] };
+                    let v3 = heap[i+3];
+                    heap[v3 as usize] = v1 + v2;
+                    i += 4;
+                },
+                2 => {
+                    let v1 = if im0 { heap[i+1] } else { heap[heap[i+1] as usize] };
+                    let v2 = if im1 { heap[i+2] } else { heap[heap[i+2] as usize] };
+                    let v3 = heap[i+3];
+                    heap[v3 as usize] = v1 * v2;
+                    i += 4;
+                },
+                3 => {
+                    let v1 = heap[i+1];
+                    heap[v1 as usize] = stdin.pop_front().unwrap();
+                    i += 2;
+                },
+                4 => {
+                    let v1 = if im0 { heap[i+1] } else { heap[heap[i+1] as usize] };
+                    stdout.push_back(v1);
+                    i += 2;
+                },
+                5 => {
+                    let v1 = if im0 { heap[i+1] } else { heap[heap[i+1] as usize] };
+                    i = if v1 != 0 {
+                        (if im1 { heap[i+2] } else { heap[heap[i+2] as usize] }) as usize
+                    } else {
+                        i + 3
+                    }
+                },
+                6 => {
+                    let v1 = if im0 { heap[i+1] } else { heap[heap[i+1] as usize] };
+                    i = if v1 == 0 {
+                        (if im1 { heap[i+2] } else { heap[heap[i+2] as usize] }) as usize
+                    } else {
+                        i + 3
+                    }
+                },
+                7 => {
+                    let v1 = if im0 { heap[i+1] } else { heap[heap[i+1] as usize] };
+                    let v2 = if im1 { heap[i+2] } else { heap[heap[i+2] as usize] };
+                    let v3 = heap[i+3];
+                    heap[v3 as usize] = if v1 < v2 { 1 } else { 0 };
+                    i += 4;
+                },
+                8 => {
+                    let v1 = if im0 { heap[i+1] } else { heap[heap[i+1] as usize] };
+                    let v2 = if im1 { heap[i+2] } else { heap[heap[i+2] as usize] };
+                    let v3 = heap[i+3];
+                    heap[v3 as usize] = if v1 == v2 { 1 } else { 0 };
+                    i += 4;
+                },
+                _ => break
+            }
+        }
+        *stdout.back().unwrap()
+    }
+}
+
--- aoc2019/src/lib.rs
+++ aoc2019/src/lib.rs
@@ -24,42 +24,5 @@ macro_rules! numbers {
     };
 }
 
-pub mod metric {
-    use std::convert::TryFrom;
-    #[derive(Copy,Clone,Debug,Default,PartialEq,Eq,PartialOrd,Ord,Hash)]
-    pub struct Point {
-        pub y: isize,
-        pub x: isize,
-    }
-    #[derive(Copy,Clone,Debug,PartialEq,Eq,PartialOrd,Ord)]
-    pub enum Direction {
-        Up,
-        Left,
-        Right,
-        Down
-    }
-    use self::Direction::*;
-
-    impl Point {
-        pub fn new<T>(y: T, x: T) -> Self where isize: TryFrom<T> {
-            Point { y: TryFrom::try_from(y).unwrap_or(0),
-                    x: TryFrom::try_from(x).unwrap_or(0) }
-        }
-        pub fn mov(&mut self, direction: Direction) {
-            match direction {
-                Up    => self.y -= 1,
-                Left  => self.x -= 1,
-                Right => self.x += 1,
-                Down  => self.y += 1
-            }
-        }
-        pub fn next_to(&self, direction: Direction) -> Point {
-            match direction {
-                Up    => Point::new(self.y - 1, self.x),
-                Left  => Point::new(self.y, self.x - 1),
-                Right => Point::new(self.y, self.x + 1),
-                Down  => Point::new(self.y + 1, self.x)
-            }
-        }
-    }
-}
+pub mod metric;
+pub mod goddamncomputer;
