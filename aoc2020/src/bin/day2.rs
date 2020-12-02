a72862f467c1c0e1d7375c7834c87db850e42bcd
Silje Raddum <kjempelodott@gmail.com>
2020-12-02 19:52:50 +0100

    use parse_display

--- aoc2020/src/bin/day2.rs
+++ aoc2020/src/bin/day2.rs
@@ -1,23 +1,30 @@
 extern crate adventofcode2020;
 use adventofcode2020::read_from_stdin;
 
+extern crate parse_display;
+use parse_display::{Display,FromStr};
+
+#[derive(FromStr,Display)]
+#[display("{x}-{y} {c}: {password}")]
+struct Input {
+    x: usize,
+    y: usize,
+    c: char,
+    password: String
+}
+
 fn main() {
     let (mut policy1, mut policy2) = (0, 0);
     for line in read_from_stdin().lines() {
-        let tok: Vec<&str> = line.split(|c: char| !c.is_alphanumeric()).collect();
-        let (x, y) = (
-            tok[0].parse::<usize>().unwrap(),
-            tok[1].parse::<usize>().unwrap()
-        );
-        let (l, pw) = (
-            tok[2].chars().nth(0).unwrap(),
-            tok[4]
-        );
-        let count = tok[4].chars().filter(|&c| c == l).count();
-        if x <= count && count <= y {
+        let i = line.parse::<Input>().unwrap();
+        let pw = i.password.as_bytes();
+        let count = pw.iter()
+            .filter(|&&c| c == i.c as u8)
+            .count();
+        if i.x <= count && count <= i.y {
             policy1 += 1;
         }
-        if (pw.chars().nth(x-1) == Some(l)) ^ (pw.chars().nth(y-1) == Some(l)) {
+        if (pw[i.x-1] == i.c as u8) ^ (pw[i.y-1] == i.c as u8) {
             policy2 += 1;
         }
     }
