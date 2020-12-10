6f1f96900d82ef2f1fed935831e8ff4b9cf5d17e
Silje Raddum <kjempelodott@gmail.com>
2020-12-10 08:25:15 +0100

    oopsie

--- aoc2020/src/bin/day9.rs
+++ aoc2020/src/bin/day9.rs
@@ -8,19 +8,21 @@ fn main() {
     let m: Vec<u64> = numbers!(read_from_stdin() => u64);
     let mut i = 0;
     while m[i..i+N].iter()
-        .find(|&k| m[i..i+N].iter().find(|&s| m[i] == s+k).is_some())
+        .find(|&k| m[i..i+N].iter().find(|&s| m[i+N] == s+k).is_some())
         .is_some() {
             i += 1;
         }
-    println!("Part 1: {}", m[i]);
+    let invalid = m[i+N];
+    println!("Part 1: {}", invalid);
+
     for i in 0..m.len() {
         let mut j = i+1;
         let mut sum = m[i];
-        while sum < m[i] {
+        while sum < invalid {
             sum += m[j];
             j += 1;
         }
-        if sum == m[i] {
+        if sum == invalid {
             let (min, max) = (&m[i..j].iter().min(), &m[i..j].iter().max());
             println!("Part 2: {}", min.unwrap() + max.unwrap());
             break;
