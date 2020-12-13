6f4e467236c9e92c1082a3672b679b4587044561
Silje Raddum <kjempelodott@gmail.com>
2020-12-13 12:39:56 +0100

    remove stupid shit

--- aoc2020/src/bin/day11.rs
+++ aoc2020/src/bin/day11.rs
@@ -1,96 +1,80 @@
 extern crate adventofcode2020;
 use adventofcode2020::read_from_stdin;
-use adventofcode2020::metric::{Point,Euclidean,Direction};
-use Direction::*;
 
 use std::collections::HashMap;
 
-#[derive(Clone,Copy,Debug,PartialEq)]
+#[derive(Clone,Copy,PartialEq)]
 enum Seat {
     Empty,
     Occupied
 }
 
 use Seat::*;
+type Point = (isize,isize);
 
 fn part1(mut seats: HashMap<Point,Seat>) -> usize {
-    loop {
-        let mut moved = HashMap::new();
-        for (&p, &s) in seats.iter() {  
-            let c = p.adjacent().iter().filter(|&a| seats.get(a) == Some(&Occupied)).count();
-            if s == Empty && c == 0 {
-                moved.insert(p, Occupied);
-            }
-            else if s == Occupied && c >= 4 {
-                moved.insert(p, Empty);
-            }
+    let adj = [(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1),(1,0),(1,1)];
+    let mut moved = true;
+    while moved {
+        moved = false;
+        let mut next = seats.clone();
+        for (&p,&s) in seats.iter() {
+            let c = adj.iter().filter(|(dy,dx)| seats.get(&(p.0+dy,p.1+dx)) == Some(&Occupied)).count();
+            moved |= match c {
+                0 if s == Empty => { next.insert(p, Occupied); true }
+                4..=8 if s == Occupied => { next.insert(p, Empty); true }
+                _ => false
+            };
         }
-        if moved.len() == 0 {
-            break;
-        }
-        for (&pos, &seat) in moved.iter() {
-            seats.insert(pos, seat);
-        }
+        seats = next;
     }
-    seats.values().filter(|&&s| s == Occupied).count()
+    seats.values().filter(|v| **v == Occupied).count()
 }
 
 fn part2(mut seats: HashMap<Point,Seat>) -> usize {
-    let ymax = seats.keys().map(|t| t.y).max().unwrap();
-    let xmax = seats.keys().map(|t| t.x).max().unwrap();
-    let directions = [Up,
-                      Down,
-                      Left,
-                      Right,
-                      Up|Left,
-                      Up|Right,
-                      Down|Right,
-                      Down|Left];
+    let ymax = seats.keys().map(|t| t.0).max().unwrap();
+    let xmax = seats.keys().map(|t| t.1).max().unwrap();
 
-    loop {
-        let mut moved = HashMap::new();
-        for (&p, &s) in seats.iter() {
+    let dir = [(-1,0),(1,0),(0,-1),(0,1),(-1,-1),(-1,1),(1,1),(1,-1)];
+    let mut moved = true;
+    while moved {
+        moved = false;
+        let mut next = seats.clone();
+        for (&p,&s) in seats.iter() {
             let mut c = 0;
-            for &d in &directions {
-                let mut l = p.clone();
-                while l.y >= 0 && l.x >=0 && l.y <= ymax && l.x <= xmax {
-                    l.mov(d);
-                    if let Some(&state) = seats.get(&l) {
-                        if state == Occupied {
-                            c += 1;
-                        }
-                        break
+            for &(dy,dx) in &dir {
+                let (mut y, mut x) = (p.0 + dy, p.1 + dx);
+                while y >= 0 && x >= 0 && y <= ymax && x <= xmax {
+                    match seats.get(&(y,x)) {
+                        Some(&Occupied) => { c += 1; break },
+                        Some(&Empty) => break,
+                        None => {}
                     }
+                    y += dy;
+                    x += dx;
                 }
             }
-            if s == Empty && c == 0 {
-                moved.insert(p, Occupied);
-            }
-            else if s == Occupied && c >= 5 {
-                moved.insert(p, Empty);
-            }
+            moved |= match c {
+                0 if s == Empty => { next.insert(p, Occupied); true }
+                5..=8 if s == Occupied => { next.insert(p, Empty); true }
+                _ => false
+            };
         }
-        if moved.len() == 0 {
-            break;
-        }
-        for (&pos, &seat) in moved.iter() {
-            seats.insert(pos, seat);
-        }
+        seats = next;
     }
-    seats.values().filter(|&&s| s == Occupied).count()
+    seats.values().filter(|v| **v == Occupied).count()
 }
 
 fn main() {
-    let seats = read_from_stdin().lines()
+let seats = read_from_stdin().lines()
         .enumerate()
-        .map(|(y,l)| l.chars()
-             .enumerate()
-             .filter(|&(_,c)| c == 'L')
-             .map(move |(x,_)| (Point::new(y,x), Empty))
+        .flat_map(|(y,l)| l.chars()
+                  .enumerate()
+                  .filter(|(_,c)| *c == 'L')
+                  .map(move |(x,_)| ((y as isize, x as isize), Empty))
         )
-        .flatten()
-        .collect::<HashMap<Point,Seat>>();
+        .collect::<HashMap<_,_>>();
     
     println!("Part 1: {}", part1(seats.clone()));
-    println!("Part 2: {}", part2(seats.clone()));
+    println!("Part 2: {}", part2(seats));
 }
