e135f1a5bd34249490f4400b67f4a4bf1d0c5c7d
Silje Raddum <kjempelodott@gmail.com>
2020-12-15 20:43:52 +0100

    day15 aoc2020

---
+++ aoc2020/src/bin/day15.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate adventofcode2020;
+use adventofcode2020::read_from_stdin;
+
+use std::collections::HashMap;
+
+fn main() {
+    let mut m: HashMap<usize,usize> = numbers!(read_from_stdin() => usize).into_iter()
+        .enumerate()
+        .map(|(i,x)| (x,i))
+        .collect();
+    let mut next = 0;
+    for i in m.len()..2019 {
+        next = if let Some(j) = m.insert(next, i) { i-j } else { 0 }
+    }
+    println!("Part 1: {}", next);
+    for i in 2019..30000000-1 {
+        next = if let Some(j) = m.insert(next, i) { i-j } else { 0 }
+    }
+    println!("Part 2: {}", next);
+}
