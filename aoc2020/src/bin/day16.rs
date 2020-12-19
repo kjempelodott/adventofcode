2c04c4cc9f0af64edd44c16ce0af53b0ed0d59c5
Silje Raddum <kjempelodott@gmail.com>
2020-12-19 10:35:13 +0100

    2020 day16

---
+++ aoc2020/src/bin/day16.rs
@@ -0,0 +1,72 @@
+#[macro_use]
+extern crate adventofcode2020;
+use adventofcode2020::{into_lines,read_from_stdin};
+
+extern crate parse_display;
+use parse_display::FromStr;
+
+#[derive(FromStr)]
+#[display("{0}-{1}")]
+struct Range(usize,usize);
+
+#[derive(FromStr)]
+#[display("{name}: {range1} or {range2}")] 
+struct Field {
+    pub name: String,
+    range1: Range,
+    range2: Range,
+    #[from_str(default)]
+    field: usize,
+}
+
+impl Field {
+    fn in_range(&self, val: usize) -> bool {
+        val >= self.range1.0 && val <= self.range1.1 ||
+            val >= self.range2.0 && val <= self.range2.1
+    }
+}
+
+fn main() {
+    let input = into_lines(read_from_stdin());
+    let mut fields: Vec<Field> = input[..20].iter()
+        .take_while(|&l| l.len() > 1)
+        .map(|l| l.parse::<Field>().unwrap())
+        .collect();
+    let mut tickets = vec![];
+    let mut error_rate = 0;
+    for l in input[23..].iter() {
+        let t = numbers!(l => usize);
+        let s: usize = t.iter()
+            .filter(|&v| fields.iter().find(|&f| f.in_range(*v)).is_none())
+            .sum();
+        if s > 0 {
+            error_rate += s;
+            continue;
+        }
+        tickets.push(t);
+    }
+    println!("Part 1: {}", error_rate);
+
+    for c in 0..fields.len() {
+        for p in 0..fields.len() {
+            if tickets.iter().all(|t| fields[p].in_range(t[c])) {
+                fields[p].field |= 1 << c;
+            }
+        }
+    }    
+    fields.sort_by_key(|f| f.field.count_ones());
+    let mut found = 0;
+    for f in fields.iter_mut() {
+        let i = &mut f.field;
+        let r = *i & found;
+        found |= *i;
+        *i = (*i^r).trailing_zeros() as usize;
+    }
+
+    let myticket = numbers!(input[21] => usize);
+    println!("Part 2: {}", fields.iter()
+             .enumerate()
+             .filter(|(_,f)| f.name.starts_with("departure"))
+             .map(|(i,_)| myticket[fields[i].field])
+             .product::<usize>());
+}
