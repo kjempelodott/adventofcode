450e77c849c8cfe51d142a15879e139569db73a5
Silje Raddum <kjempelodott@gmail.com>
2020-12-22 12:34:50 +0100

    cleanup day21

--- aoc2020/src/bin/day21.rs
+++ aoc2020/src/bin/day21.rs
@@ -4,59 +4,46 @@
 extern crate adventofcode2020;
 use adventofcode2020::read_from_stdin;
 
-use std::collections::{BTreeMap,HashSet};
+use std::collections::{BTreeMap,HashMap,HashSet};
 
 fn main() {
-    let foods = read_from_stdin().lines()
-        .map(|line| {
-            let (list, allergens) = line.split_once('(').unwrap();
-            (list.trim().split(' ')
-             .map(|s| s.to_string())
-             .collect::<Vec<String>>(),
-             allergens.trim_matches(')').split(' ')
-             .skip(1)
-             .map(|s| s.trim_matches(',').to_string())
-             .collect::<Vec<String>>())
-        })
-        .collect::<Vec<_>>();
+    let mut allergen_map: BTreeMap<String,HashSet<String>> = BTreeMap::new();
+    let mut i_count: HashMap<String,usize> = HashMap::new();
 
-    let ingredients: HashSet<&String> = foods.iter().map(|(i,_)| i.iter()).flatten().collect();
-    let mut allergens: BTreeMap<&String,HashSet<&String>> = BTreeMap::new();
+    for l in read_from_stdin().lines() {
+        let (isub, asub) = l.split_once(" (contains ").unwrap();
+        let ingredients = isub.split(' ')
+            .map(|s| s.to_string())
+            .collect::<HashSet<_>>();
+        let allergens = asub.trim_matches(')').split(", ")
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
 
-    for (ingrs,alrgs) in foods.iter() {
-        for alrg in alrgs.iter() {
-            if let Some(seen) = allergens.get_mut(&alrg) {
-                seen.drain_filter(|i| !ingrs.contains(&i));
-            }
-            else {
-                allergens.insert(&alrg, ingrs.iter().collect());
-            }
+        for i in ingredients.iter() {
+            *i_count.entry(i.to_string()).or_default() += 1;
+        }
+        for a in allergens.iter() {
+            allergen_map.entry(a.to_string())
+                .and_modify(|x| { x.drain_filter(|i| !ingredients.contains(i)); } )
+                .or_insert(ingredients.clone());
         }
     }
+    println!("Part 1: {}", i_count.iter()
+             .filter(|(i,_)| !allergen_map.values().any(|a| a.contains(*i)))
+             .map(|(_,v)| v)
+             .sum::<usize>());
 
-    println!("Part 1: {}", ingredients.iter()
-             .map(|i|
-                  if !allergens.values().any(|a| a.contains(i)) {
-                      foods.iter().filter(|(x,_)| x.contains(i)).count()
-                  }
-                  else {
-                      0
-                  })
-             .sum::<usize>());    
-
-    let mut mapped = allergens.values()
+    let mut mapped = allergen_map.values()
         .filter(|a| a.len() == 1)
         .map(|a| a.iter().next().unwrap().to_string())
         .collect::<Vec<_>>();
-
-    while let Some(a) = mapped.pop() {
-        for alrgs in allergens.values_mut().filter(|a| a.len() > 1) {
-            if alrgs.remove(&a) && alrgs.len() == 1 {
-                mapped.push(alrgs.iter().next().unwrap().to_string())
-            }
+    for a in allergen_map.values_mut().filter(|a| a.len() > 1) {
+        a.drain_filter(|x| mapped.contains(&x));
+        if a.len() == 1 {
+            mapped.push(a.iter().next().unwrap().to_string());
         }
     }
-    println!("Part 2: {}", allergens.values()
+    println!("Part 2: {}", allergen_map.values()
              .map(|v| v.iter().next().unwrap().to_string())
              .collect::<Vec<_>>()
              .join(","));
