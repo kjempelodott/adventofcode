2c527a78f393d7d0068364aa79244f098be2e66f
Silje Raddum <kjempelodott@gmail.com>
2021-12-01 14:32:48 +0100

    day1

---
+++ aoc2021/src/bin/day1.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate aoc2021;
+use aoc2021::read_from_stdin;
+
+extern crate itertools;
+use itertools::Itertools;
+
+fn main() {
+    let n: Vec<u64> = numbers!(read_from_stdin() => u64);
+    println!("Part 1: {}", n.iter().skip(1)
+             .zip(n.iter())
+             .filter(|(&c,&d)| c > d).count());
+    println!("Part 2: {}", n.iter().skip(1).tuple_windows::<(_,_,_)>()
+             .zip(n.iter().tuple_windows::<(_,_,_)>())
+             .filter(|(v,w)| v.0+v.1+v.2 > w.0+w.1+w.2).count());
+}
