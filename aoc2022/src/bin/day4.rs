96a2fa0e2a9a9f62aadf1718cc47164018dd8d61
Silje Raddum <8458122+kjempelodott@users.noreply.github.com>
2022-12-04 16:31:47 +0100

    spaces

--- aoc2022/src/bin/day4.rs
+++ aoc2022/src/bin/day4.rs
@@ -6,7 +6,7 @@ fn main() {
         let s = line.split(|c: char| !c.is_digit(10))
             .filter_map(|l| l.parse::<usize>().ok())
             .collect::<Vec<_>>();
-        let (l1,r1,l2,r2) = (s[0], s[1], s[2], s[3]);
+        let (l1, r1, l2, r2) = (s[0], s[1], s[2], s[3]);
         if (l1 <= l2 && r1 >= r2) || (l2 <= l1 && r2 >= r1) {
             return (s1 + 1, s2 + 1)
         }
