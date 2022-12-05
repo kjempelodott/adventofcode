41b52592356773df95ac06a93a0eb19cd095071f
Silje Raddum <8458122+kjempelodott@users.noreply.github.com>
2022-12-05 23:11:26 +0100

    use fold

--- aoc2022/src/bin/day1.rs
+++ aoc2022/src/bin/day1.rs
@@ -3,15 +3,13 @@ use aoc2022::read_from_stdin;
 
 fn main() {
     let mut calories = vec![];
-    let mut sum = 0;
-    for line in read_from_stdin().lines() {
+    read_from_stdin().lines().fold(0, |sum, line| {
         if line.is_empty() {
             calories.push(sum);
-            sum = 0;
-            continue;
+            return 0
         }
-        sum += line.parse::<usize>().unwrap();
-    }
+        sum + line.parse::<usize>().unwrap()
+    });
     calories.sort();
     println!("Part 1: {}", calories.last().unwrap());
     println!("Part 2: {}", calories.iter().rev().take(3).sum::<usize>());
